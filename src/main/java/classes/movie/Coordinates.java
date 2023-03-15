ab3ec82059e3b1ed1dfc24ab20cd961869aed41c
Nikulin Vasiliy <nikulin.vasily.777@ya.ru>
2023-03-15 19:31:53 +0300

    Added count_by_oscars_count command (#32)

--- src/main/java/classes/movie/Coordinates.java
+++ src/main/java/classes/movie/Coordinates.java
@@ -15,6 +15,9 @@ public class Coordinates implements Comparable<Coordinates> {
     private long x; //Максимальное значение поля: 279
     private int y; //Значение поля должно быть больше -230
 
+    private Coordinates() {
+    }
+
     public Coordinates(long x, int y) throws NotGreatThanException, NullValueException, GreatThanException {
         this.x = new FieldHandler(x, MAX_VALUE).handleLong();
         this.y = new FieldHandler(y, GREAT_THAN_X).handleInt();
