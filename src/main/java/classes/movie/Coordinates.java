36dc8fdb47786f5d04308319bdd3c02e927cb4c9
Poolyakoffffffff <59867043+GreedlyCore@users.noreply.github.com>
2023-03-15 18:58:40 +0300

    Added remove_lower command (#29)
    * update coordinates
    
    * fix compareTo in Coordinates class
    
    * add RemoveLower command
    
    * code cleanup
    
    * Update RemoveLower.java
    
    * Update Coordinates.java
    
    ---------
    
    Co-authored-by: Nikulin Vasiliy <nikulin.vasily.777@ya.ru>

--- src/main/java/classes/movie/Coordinates.java
+++ src/main/java/classes/movie/Coordinates.java
@@ -11,12 +11,9 @@ import static classes.movie.FieldProperty.MAX_VALUE;
  * Model of Coordinates. Sub-model of the <code>Route</code>. Contains getters/setters of each class fields.
  * Some fields have restrictions. It's signed under every method of field.
  */
-public class Coordinates {
+public class Coordinates implements Comparable<Coordinates> {
     private long x; //Максимальное значение поля: 279
     private int y; //Значение поля должно быть больше -230
-
-    private Coordinates() {
-    }
 
     public Coordinates(long x, int y) throws NotGreatThanException, NullValueException, GreatThanException {
         this.x = new FieldHandler(x, MAX_VALUE).handleLong();
@@ -53,8 +50,8 @@ public class Coordinates {
     public boolean equals(Object obj) {
         return (
                 obj instanceof Coordinates &&
-                        this.getX() == ((Coordinates) obj).getX() &&
-                        this.getY() == ((Coordinates) obj).getY()
+                this.getX() == ((Coordinates) obj).getX() &&
+                this.getY() == ((Coordinates) obj).getY()
         );
     }
 
@@ -63,4 +60,12 @@ public class Coordinates {
         return "(" + x + ", " + y + ")";
     }
 
+    @Override
+    public int compareTo(Coordinates o) {
+        if ((getX() == o.getX()) && (getY()==o.getY()))
+            return 0;
+        else if (getX() != o.getX())
+                return (int) (o.getX() - getX());
+             return o.getY() - getY();
+    }
 }
