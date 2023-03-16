f7a985e4f9d45919ca6a5dab313ac936ec614a19
GreedlyCore <naganariselves@gmail.com>
2023-03-16 22:07:50 +0300

    decompose sort collection method for commands: - Sort - PrintDescending

--- src/main/java/classes/collection/CollectionManager.java
+++ src/main/java/classes/collection/CollectionManager.java
@@ -1,10 +1,10 @@
 package classes.collection;
 
+import classes.console.TextColor;
+import classes.movie.Coordinates;
 import classes.movie.Movie;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 public class CollectionManager {
     private static ArrayList<Movie> collection = null;
@@ -49,7 +49,30 @@ public class CollectionManager {
         collection.remove(movieIndex);
     }
 
-    public void clear(){
+    public void clear() {
         collection.clear();
+    }
+
+    public static void sort(ArrayList<Movie> collection) {
+
+        List<Coordinates> moviesCoordinatesList = new ArrayList<>();
+        for (Movie movies : collection)
+            moviesCoordinatesList.add(movies.getCoordinates());
+        moviesCoordinatesList.sort((o1, o2) -> {
+            Long x1 = o1.getX();
+            Long y1 = (long) o1.getY();
+            Long x2 = o2.getX();
+            Long y2 = (long) o2.getY();
+            return !x1.equals(x2) ? x1.compareTo(x2) : y1.compareTo(y2);
+        });
+        for (int i = 0; i < moviesCoordinatesList.size(); i++) {
+            Coordinates m = moviesCoordinatesList.get(i);
+
+            for (int j = 0; j < collection.size(); j++) {
+                Movie movies = collection.get(j);
+                if (m.equals(movies.getCoordinates()))
+                    Collections.swap(collection, i, j);
+            }
+        }
     }
 }
