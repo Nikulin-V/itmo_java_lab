7f92a4787690bd28a2ead5bd84209af038c36561
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2023-05-22 18:05:03 +0300

    Added ClientSession.authorize

--- src/main/java/classes/ClientSession.java
+++ src/main/java/classes/ClientSession.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
+import java.util.UUID;
 
 public class ClientSession implements Runnable {
     private final Socket socket;
@@ -16,29 +17,74 @@ public class ClientSession implements Runnable {
         this.socket = socket;
     }
 
+
+    private UUID authorize(ObjectInputStream in, ObjectOutputStream out) throws IOException {
+        String userChoice = "";
+        while (!(userChoice.equals("1") || userChoice.equals("2"))) {
+            out.writeUTF("1 - Войти (по умолчанию)\n2 - Зарегистрироваться");
+            out.flush();
+            userChoice = in.readUTF();
+        }
+        if (userChoice.equals("2"))
+            return register(in, out);
+        else return login(in, out);
+    }
+
+    private UUID login(ObjectInputStream in, ObjectOutputStream out) throws IOException {
+        while (true) {
+            out.writeUTF("Логин: ");
+            out.flush();
+            String login = in.readUTF();
+            out.writeUTF("Пароль: ");
+            out.flush();
+            String password = in.readUTF();
+
+            if (checkLogin(login)) {
+                UUID userID = checkPassword(password);
+                if (userID != null)
+                    return userID;
+                else System.out.println(TextColor.red("Неверный пароль\n"));
+            } else System.out.println(TextColor.yellow("Пользователь с таким логином не существует\n"));
+        }
+    }
+
+    private UUID checkPassword(String password) {
+        UUID userID;
+        return userID;
+    }
+
+    private boolean checkLogin(String login) {
+        return false;
+    }
+
+    private UUID register(ObjectInputStream in, ObjectOutputStream out) {
+        // TODO: write user registration code
+        UUID userID;
+        return userID;
+    }
+
     @Override
     public void run() {
-        try {
+        try (
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())
+        ) {
             System.out.println(TextColor.grey("Соединение установлено: " + socket.getInetAddress()));
+            UUID currentUserID = authorize(inputStream, outputStream);
 
             while (!socket.isClosed()) {
-
-                    Object inputObject = inputStream.readObject();
-                    if (inputObject instanceof NamedCommand command) {
-                        Response outputData;
-                        if (command.hasTransferData()) {
-                            Object inputData = inputStream.readObject();
-                            outputData =  command.execute(inputData);
-                        } else
-                            outputData = command.execute(null);
-                        if (outputData != null)
-                            outputStream.writeObject(outputData);
-                    } else outputStream.writeObject(new Response(1).setData(TextColor.yellow("Передана неизвестная команда")));
-
-                    outputStream.flush();
-
+                Object inputObject = inputStream.readObject();
+                if (inputObject instanceof NamedCommand command) {
+                    Response outputData;
+                    if (command.hasTransferData()) {
+                        Object inputData = inputStream.readObject();
+                        outputData =  command.execute(inputData);
+                    } else
+                        outputData = command.execute(null);
+                    if (outputData != null)
+                        outputStream.writeObject(outputData);
+                } else outputStream.writeObject(new Response(1).setData(TextColor.yellow("Передана неизвестная команда")));
+                outputStream.flush();
             }
         } catch (IOException | ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
                  InstantiationException | IllegalAccessException e) {
