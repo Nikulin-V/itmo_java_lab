37a1f72cd720eb76c569e5a9e81863b308696e7f
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2023-06-11 00:01:42 +0300

    Added thread pools and objects & command buffers

--- src/main/java/classes/ClientSession.java
+++ src/main/java/classes/ClientSession.java
@@ -8,15 +8,17 @@ import classes.sql_managers.SQLUserManager;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
+import java.util.ArrayDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ClientSession implements Runnable {
     private final Socket socket;
-    private final static ExecutorService outputFixedThreadPool = Executors.newFixedThreadPool(10);
-    private final static ExecutorService inputCachedThreadPool = Executors.newCachedThreadPool();
+    public final static ExecutorService outputFixedThreadPool = Executors.newFixedThreadPool(10);
+    public final static ExecutorService inputCachedThreadPool = Executors.newCachedThreadPool();
+    public final ArrayDeque<Object> objectsQueue = new ArrayDeque<>();
+    public final ArrayDeque<Object> commandsQueue = new ArrayDeque<>();
 
     public ClientSession(Socket socket) {
         this.socket = socket;
@@ -24,7 +26,7 @@ public class ClientSession implements Runnable {
 
     private boolean checkPassword(UserCredentials userCredentials) {
         String password = SQLManager.executePreparedQueryGetPassword(userCredentials.getUsername());
-        if (password != null ) {
+        if (password != null) {
             return password.equals(userCredentials.getHashedPassword());
         }
         return false;
@@ -32,18 +34,6 @@ public class ClientSession implements Runnable {
 
     private boolean isLoginInDB(String username) {
         return SQLManager.executeCheckLogin(username);
-    }
-
-    private Response executeCommand(ObjectInputStream inputStream,
-                                    UserCredentials userCredentials) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        Object inputObject = inputStream.readObject();
-        if (inputObject instanceof NamedCommand command) {
-            if (command.hasTransferData()) {
-                Object inputData = inputStream.readObject();
-                return command.execute(inputData, userCredentials.getUsername());
-            } else return command.execute(null, userCredentials.getUsername());
-        }
-        return new Response(1, TextColor.yellow("Передана неизвестная команда"));
     }
 
     @Override
@@ -55,32 +45,26 @@ public class ClientSession implements Runnable {
             System.out.println(TextColor.grey("Соединение установлено: " + socket.getInetAddress()));
             UserCredentials currentUserCredentials = authorize(inputStream, outputStream);
             while (!socket.isClosed()) {
-                inputCachedThreadPool.execute(() -> {
-                    try {
-                        Object inputObject = inputStream.readObject();
-                        if (inputObject instanceof UserCredentials credentials) {
-                            outputFixedThreadPool.execute(() -> {
-                                try {
-                                    if (currentUserCredentials.equals(credentials)) {
-                                        outputStream.writeObject(executeCommand(inputStream, currentUserCredentials));
-                                    } else
-                                        outputStream.writeObject(new Response(1, TextColor.red("Попытка подмены данных пользователя")));
-                                    outputStream.flush();
-                                } catch (IOException | ClassNotFoundException | InvocationTargetException |
-                                         NoSuchMethodException | InstantiationException | IllegalAccessException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            });
-                        }
-                    } catch (IOException e) {
-                        System.out.println(TextColor.grey("Соединение разорвано, ожидаю нового подключения"));
-                    } catch (ClassNotFoundException e) {
-                        System.out.println(TextColor.red("Получены неизвестные данные"));
-                    }
-                });
+                readFullRequest(inputStream);
+                inputCachedThreadPool.execute(new ReadQueryTask(this, currentUserCredentials, outputFixedThreadPool, outputStream));
             }
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println(TextColor.grey("Соединение разорвано, ожидаю нового подключения"));
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void readFullRequest(ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
+        Object credentials;
+        Object inputObject;
+        credentials = inputStream.readObject();
+        if (credentials instanceof UserCredentials) {
+            inputObject = inputStream.readObject();
+            if (inputObject instanceof NamedCommand command) {
+                objectsQueue.add(credentials);
+                objectsQueue.add(command);
+                if (command.hasTransferData())
+                    objectsQueue.add(inputStream.readObject());
+            }
         }
     }
 
